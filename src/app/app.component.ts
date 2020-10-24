74992191ef7b13edc4ecbd2add2dc5c478ef397e
tanishq <tanishqsinghai11@gmail.com>
2020-10-24 20:01:40 +0530

    wikiAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { WikipediaService } from './wikipedia.service';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wiki';
+  pages=[];
+  constructor(private wikipedia: WikipediaService){
+
+  }
+
+  onTerm(term:string) {
+    this.wikipedia.search(term).subscribe((response:any) => {
+      this.pages=response.query.search;
+
+    });
+    
+  }
 }
